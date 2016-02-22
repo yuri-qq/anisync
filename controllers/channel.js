265b241be34c49177fd01898aeddc613f168473c
Yurifag <jannesgrzebien@gmail.com>
2016-02-22 21:36:15 +0100

    put site specific script tags in views and append head instead of passing them in from controller

--- controllers/channel.js
+++ controllers/channel.js
@@ -1,7 +1,7 @@
 var Channel = require('../models/channel');
 
 module.exports.form = function(req, res) {
-  res.render("create", {data: {javascripts: ["/js/create.js"]}});
+  res.render("create");
 };
 
 module.exports.create = function(req, res) {
@@ -54,27 +54,7 @@ module.exports.join = function(req, res, next) {
 
     function renderView() {
       if(req.session.username && (req.session.loggedInId == req.params.id || !data.private)) {
-        var javascripts = [
-          "https://cdn.socket.io/socket.io-1.4.5.js",
-          "https://fb.me/react-0.14.7.min.js",
-          "https://fb.me/react-dom-0.14.7.js",
-          "/js/lib/video.js",
-          "//cdnjs.cloudflare.com/ajax/libs/Sortable/1.4.2/Sortable.min.js",
-          "/js/lib/react-sortable-mixin.js",
-          "/js/components/Channel/Video/VideoApp.js",
-          "/js/components/Channel/Playlist/PlaylistControls.js",
-          "/js/components/Channel/Playlist/PlaylistItem.js",
-          "/js/components/Channel/Playlist/Playlist.js",
-          "/js/components/Channel/Playlist/PlaylistApp.js",
-          "/js/components/Channel/Chat/ChatControls.js",
-          "/js/components/Channel/Chat/ChatMessage.js",
-          "/js/components/Channel/Chat/ChatApp.js",
-          "/js/components/Channel/Users/UserItem.js",
-          "/js/components/Channel/Users/UserApp.js",
-          "/js/components/Channel/App.js",
-          "/js/channel.js"
-        ];
-        res.render("channel", {data: {javascripts: javascripts, name: data.name}});
+        res.render("channel", {data: {name: data.name}});
       }
       else {
         res.render("join", {data: {private: data.private, username: req.session.username ? true : false, errors: errors}});
--- controllers/site.js
+++ controllers/site.js
@@ -6,18 +6,7 @@ module.exports.index = function(req, res) {
     var username = "";
   }
 
-  var javascripts = [
-    "https://cdn.socket.io/socket.io-1.4.5.js",
-    "https://fb.me/react-0.14.7.min.js",
-    "https://fb.me/react-dom-0.14.7.js",
-    "/js/components/Index/ChannelList/ChannelControls.js",
-    "/js/components/Index/ChannelList/ChannelItem.js",
-    "/js/components/Index/ChannelList/ChannelListApp.js",
-    "/js/components/Index/App.js",
-    "/js/index.js"
-  ];
-
-  res.render("index", {data: {javascripts: javascripts, username: username}});
+  res.render("index", {data: {username: username}});
 };
 
 module.exports.about = function(req, res) {
